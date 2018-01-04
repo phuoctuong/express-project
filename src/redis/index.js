1b6b50691be64a57ba49afb726140cc8472e4aa6
PhuocTuong <phuoctuong285@gmail.com>
2018-01-04 20:34:28 +0700

    [007] Configure redis

---
+++ src/redis/index.js
@@ -0,0 +1,30 @@
+// @flow
+
+import redis from 'redis';
+import config from '../config/db';
+import logger from '../helper/logger';
+
+let redisClient;
+
+if (!redisClient) {
+	redisClient = redis.createClient({
+		host: config.redis.host,
+		port: config.redis.port
+	});
+
+	redisClient.auth(config.redis.password, (err) => {
+		if (err) {
+			logger.error(`Error Auth in Redis ${err}`);
+		}
+	});
+
+	redisClient.on('ready', () => {
+		logger.info('Redis is ready');
+	});
+
+	redisClient.on('error', (err) => {
+		logger.error(`Error in Redis ${err}`);
+	});
+}
+
+export default redisClient;
